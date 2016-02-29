391a3cf70d78331bf812229d7701323aa12bf25c
Maxim Kornienko <maxim.kornienko@outlook.com>
2016-02-29 23:02:48 +0100

    Speed Limit

---
+++ src/main/java/SpeedLimit.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://open.kattis.com/problems/speedlimit
+ */
+public class SpeedLimit {
+
+    private int totalDistance;
+
+    private int totalElapsedTime;
+
+    public void addLog(int speed, int elapsedTime) {
+        totalDistance += (elapsedTime - totalElapsedTime) * speed;
+        totalElapsedTime = elapsedTime;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in), 8092);
+
+        int logsCount = Integer.parseInt(in.readLine());
+        while (logsCount != -1) {
+            SpeedLimit result = new SpeedLimit();
+            for (int i = 0; i < logsCount; i++) {
+                String[] logLine = in.readLine().split(" ");
+                result.addLog(Integer.parseInt(logLine[0]), Integer.parseInt(logLine[1]));
+            }
+            System.out.println(result.getTotalDistance() + " miles");
+            logsCount = Integer.parseInt(in.readLine());
+        }
+    }
+}
