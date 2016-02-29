a64e78b9a560663c500ea612f3c44fb753f6b74a
Maxim Kornienko <maxim.kornienko@outlook.com>
2016-02-29 23:30:37 +0100

    Simon Says

---
+++ src/main/java/SimonSays.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://open.kattis.com/problems/simon
+ */
+public class SimonSays {
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in), 8092);
+    private static String MAGIC_WORD = "simon says ";
+
+    public static void main(String[] args) throws IOException {
+        int testCasesCount = Integer.parseInt(in.readLine());
+        for (int t = 0; t < testCasesCount; t++) {
+            String phrase = in.readLine();
+            System.out.println(getCommand(phrase));
+        }
+    }
+
+    public static String getCommand(String phrase) {
+        int pos = phrase.indexOf(MAGIC_WORD);
+        if (pos == -1)
+            return "";
+        return phrase.substring(pos + MAGIC_WORD.length());
+    }
+
+}
