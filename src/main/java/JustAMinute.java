bfdeb38254c2cd7aabfc91e5597aee7d3b88e70d
Maxim Kornienko <maxim.kornienko@outlook.com>
2016-03-03 22:43:43 +0100

    Just a Minute

---
+++ src/main/java/JustAMinute.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://open.kattis.com/problems/justaminute
+ */
+public class JustAMinute {
+
+    static final double ERROR = Math.pow(10, -7);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in), 8092);
+        int observationsCount = Integer.parseInt(in.readLine());
+        int minutes = 0;
+        int seconds = 0;
+        for (int i = 0; i < observationsCount; i++) {
+            String[] observation = in.readLine().split(" ");
+            minutes += Integer.parseInt(observation[0]);
+            seconds += Integer.parseInt(observation[1]);
+        }
+        double result = seconds / (minutes * 60.0);
+        if (result <= (1 + ERROR)) {
+            System.out.println("measurement error");
+        } else {
+            System.out.println(result);
+        }
+
+    }
+}
