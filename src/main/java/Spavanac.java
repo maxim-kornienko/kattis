6c7cff051ff7cd1548222f4837d77919513ae640
Maxim Kornienko <maxim.kornienko@outlook.com>
2016-03-08 01:03:42 +0100

    Spavanac

---
+++ src/main/java/Spavanac.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * https://open.kattis.com/problems/spavanac
+ */
+public class Spavanac {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int hours = s.nextInt();
+        int minutes = s.nextInt();
+
+        int time = hours * 60 + minutes;
+        time -= 45;
+        if (time < 0)
+            time += 24 * 60;
+        System.out.printf("%d %d", time / 60, time % 60);
+    }
+}
