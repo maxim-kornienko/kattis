6d555774d0ca12c08d2155e6f0c71696881481e5
Maxim <maxim.kornienko@outlook.com>
2018-03-07 08:20:55 +0100

    Pot

--- kattis-python/Pot.py
+++ kattis-python/Pot.py
@@ -1,12 +1,20 @@
 import sys
 
-result = 0
+def main():
+    """"
+    https://open.kattis.com/problems/pot
+    :returns: None
+    """
+    result = 0
 
-n = int(sys.stdin.readline())
-for i in range(0, n):
-    p = int(sys.stdin.readline())
-    number = p // 10
-    power = p % 10
-    result += pow(number, power)
+    n = int(sys.stdin.readline())
+    for i in range(0, n):
+        p = int(sys.stdin.readline())
+        number = p // 10
+        power = p % 10
+        result += pow(number, power)
 
-print(result)
+    print(result)
+
+if  __name__ =='__main__':main()
+
